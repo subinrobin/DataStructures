6595a1ad298a392fde793102b94e12fe991bbdb8
Subin <subinrobin@users.noreply.github.com>
2021-06-13 13:19:20 +0530

    solution for Maximize the sum of array elements problem

--- src/main/scala/datastructures/arrays/MinimumAndXorOr.scala
+++ src/main/scala/datastructures/arrays/MinimumAndXorOr.scala
@@ -1,15 +1,22 @@
 package datastructures.arrays
 
-object MinimumAndXorOr extends App {
+
+
+object MaximizeTheSum extends App {
     val testCases = scala.io.StdIn.readInt()
     for (i <- 1 to testCases) {
-        var result = Int.MaxValue
-        val arrSize = scala.io.StdIn.readInt()
-        val inputArr = scala.io.StdIn.readLine().split(" ").map(_.toInt).sorted // sorting the input
-        for (j <- 0 to arrSize - 2) { // finding xor of consecutive elements
-            val t = inputArr(j) ^ inputArr(j + 1)
-            result = result.min(t)
-        }
-        println(result)
+        val args2 = scala.io.StdIn.readLine().split(" ").map(_.toInt)
+        val arrSize = args2(0)
+        val noOfElems = args2(1)
+        val elems = scala.io.StdIn.readLine().split(" ").map(_.toLong)
+        val tracker = scala.collection.mutable.Map[Long, Long]()
+        
+        elems.foreach(c => {
+            if(c > 0) {
+                tracker.put(c, tracker.getOrElse(c, 0L) + c)
+            }
+        })
+        val elemSum = tracker.values.toArray.sorted
+        println(elemSum.takeRight(noOfElems).sum)
     }
 }
